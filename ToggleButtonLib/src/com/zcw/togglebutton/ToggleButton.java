8fdba36f53bfac3ee32482fb243d9e7f0f798a9c
dongchengXie <383333166@qq.com>
2015-05-21 17:50:46 +0800

    discovered a small bug
    I try to use "wrap_content" in the layout xml file, and then discovered
    this bug.

--- ToggleButtonLib/src/com/zcw/togglebutton/ToggleButton.java
+++ ToggleButtonLib/src/com/zcw/togglebutton/ToggleButton.java
@@ -194,7 +194,7 @@ public class ToggleButton extends View{
 			widthMeasureSpec = MeasureSpec.makeMeasureSpec(widthSize, MeasureSpec.EXACTLY);
 		}
 		
-		if(heightSize == MeasureSpec.UNSPECIFIED || heightSize == MeasureSpec.AT_MOST){
+		if(heightMode == MeasureSpec.UNSPECIFIED || heightSize == MeasureSpec.AT_MOST){
 			heightSize = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, r.getDisplayMetrics());
 			heightMeasureSpec = MeasureSpec.makeMeasureSpec(heightSize, MeasureSpec.EXACTLY);
 		}
