ddd54dd5e143067a6f01413fa0a08c6004a3f951
薛小龙 <setsunal@outlook.com>
2016-11-24 17:45:35 +0800

    新增了获得开关的状态和用布尔值控制开关的状态

--- togglebutton-library/src/com/zcw/togglebutton/ToggleButton.java
+++ togglebutton-library/src/com/zcw/togglebutton/ToggleButton.java
@@ -324,5 +324,15 @@ public class ToggleButton extends View{
 	public void setAnimate(boolean animate) {
 		this.defaultAnimate = animate;
 	}
-	
+	public void setChecked(boolean isChecked){
+		if (isChecked){
+			toggle();
+		}else {
+			toggleOff();
+		}
+	}
+
+	public boolean isChecked(){
+		return toggleOn;
+	}
 }
